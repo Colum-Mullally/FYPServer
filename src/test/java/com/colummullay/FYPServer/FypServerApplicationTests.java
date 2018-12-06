c53351cacce125b63a18d0f5f6cf44e78e371d89
Colum Mullally <15142434@studentmail.ul.ie>
2018-12-06 05:18:00 +0000

    Init

---
+++ src/test/java/com/colummullay/FYPServer/FypServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.colummullay.FYPServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FypServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
